dbee01fcf3488c5a7f8d76b66dce1281aa944911
Coursera Learner <nobody@nowhere.nul>
2018-06-01 17:42:58 +0000

    working copy

---
+++ 08_testing/test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+int prime_correct(int a){
+  
+
+  /*  int p = 32;
+  int s = system("isPrime-correct %d",p);
+  printf("%d", s);
+  return 0;
+  */
+
+  char command[100];  
+  a = 42;  
+  sprintf(command, "./isPrime-correct %d", a);  
+  int s = system(command);
+  printf("%d", s);
+  return s;
+}
+
+int main(){
+  prime_correct(30);
+  }
+
+ 
