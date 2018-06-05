2cb96e3d1296a674e2107f0325ba810370e81907
Coursera Learner <nobody@nowhere.nul>
2018-06-05 21:02:06 +0000

    test file

--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,28 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
 #include "cards.h"
 
+
 int main(void) {
+card_t my_card = card_from_letters('7', 'h');
+ 
+print_card(my_card);
+ printf("\n");
+
+ 
+
+ for(int i = 0; i < 52; i++){
+   my_card = card_from_num(i);
+   print_card(my_card);
+   printf("\n");
+   }
+ //my_card = card_from_letters('%', 'h');
+ //print_card(my_card);
+ //printf("\n");
+ 
+
+
+
 
 }
